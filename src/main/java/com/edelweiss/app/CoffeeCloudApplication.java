79bfe641c113b5b52fe43018d4e0fc823ffb28d4
humbl3LilaS <sabishinekobebe@gmail.com>
2024-08-09 12:17:40 +0630

    initial setup

---
+++ src/main/java/com/edelweiss/app/CoffeeCloudApplication.java
@@ -0,0 +1,15 @@
+package com.edelweiss.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeCloudApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CoffeeCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edelweiss/app/CoffeeCloudApplicationTests.java
@@ -0,0 +1,15 @@
+package com.edelweiss.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeCloudApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
